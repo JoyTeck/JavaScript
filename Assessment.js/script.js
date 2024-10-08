6325ce880fba8797f01e2ead67eb02ea44665966
JoyTeck <bukunmioloruntobi@gmail.com>
2024-10-08 06:47:46 +0100

    modify Assessment.js/script.js file

--- Assessment.js/script.js
+++ Assessment.js/script.js
@@ -1,5 +1,26 @@
-let name = "john";
-let surname = "Doe";
+function calculate(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+    case "/":
+      if (num2 !== 0) {
+        return num1 / num2;
+      } else {
+        return "Error: Division by zero";
+      }
+    case "%":
+      return num1 % num2;
+    default:
+      return "Error: Invalid operator";
+  }
+}
 
-console.log(name);
-console.log(surname);
+console.log(calculate(10, 5, "+")); // 15
+console.log(calculate(10, 5, "-")); // 5
+console.log(calculate(10, 5, "*")); // 50
+console.log(calculate(10, 5, "/")); // 2
+console.log(calculate(10, 5, "%")); // 0
