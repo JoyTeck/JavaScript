61137f505f8c09ee79e44d588f0cee694b7e8f63
JoyTeck <bukunmioloruntobi@gmail.com>
2024-10-14 06:38:04 +0100

    edit Assessment.js/script.js and added Assessment.js/script2.js

--- Assessment.js/script.js
+++ Assessment.js/script.js
@@ -26,7 +26,4 @@ console.log(calculate(10, 5, "-")); // 5
 console.log(calculate(10, 5, "*")); // 50
 console.log(calculate(10, 5, "/")); // 2
 console.log(cal.toFixed(2));
-console.log;
 console.log(calculate(10, 5, "%")); // 0
-
-//Question 2 : Create a single function that takes an array of numbers and operators as input, and perform the operations in chronological
---
+++ Assessment.js/script2.js
@@ -0,0 +1,1 @@
+//Question 2 : Create a single function that takes an array of numbers and operators as input, and perform the operations in chronological
